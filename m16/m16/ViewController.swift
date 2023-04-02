0b0b950df1d416b9ba109f99deb581a8f433401c
Nursultan Konspayev <nursultan.konspayev@gmail.com>
2023-04-02 10:15:06 +0600

    Adding Xcode Project

---
+++ m16/m16/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  m16
+//
+//  Created by Nursultan Konspayev on 02.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
